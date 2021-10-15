ea8e53db6b9806f6f4170cba94834fdbf01071ba
k-nautresurrect <kushignite@gmail.com>
2021-10-15 21:54:37 +0530

    Modules export.

--- app.js
+++ app.js
@@ -8,9 +8,36 @@ var instrinct = 19;
 // variable nature in javascript
 
 // let 
+console.log(module);
+const model = require("./model");
+// import "model.js";   // ES6 -> ECMA script 6 -> 2015 version
 instrinct = "string!!"
 function hello(name){
     console.log("hello "+name);
 }
+model.message("testcase");
+//module exported.
 
-hello(instrinct)
+// io intensive work  = 2*java,python.
+// scope of javascript
+// {
+
+// }
+
+
+// {
+
+// }
+
+
+    // req <-  thread ---->  cpu(execute)
+    // req <-  thread(2nd time) ---> cpu (execute)
+    // req <-  thread(3rd time) ---> cpu (execute)
+
+    // input output kbhi bnd ni hora
+
+    // 1st thread operation result -> | 1 , 2, 3,event queue |
+
+
+
+
---
+++ model.js
@@ -0,0 +1,8 @@
+function message(message){
+    console.log(message);
+    console.log(module);
+}
+
+module.exports.message = message;
+
+// module . export . variable name = (attribute, func, const,..)
