50d42f7619fda588fd7acead846abbf6c5dd5526
k-nautresurrect <kushignite@gmail.com>
2021-10-15 21:27:02 +0530

    Node intro

---
+++ app.js
@@ -0,0 +1,16 @@
+global.console.log("hello world!")
+// some module differ from javascript
+// what is module
+// module is file
+// dynamically typed language
+// integer double string defined undefined array object null
+var instrinct = 19;
+// variable nature in javascript
+
+// let 
+instrinct = "string!!"
+function hello(name){
+    console.log("hello "+name);
+}
+
+hello(instrinct)
