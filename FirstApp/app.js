8066140f740af25110b2f09eb90a8503db270d8f
k-nautresurrect <kushignite@gmail.com>
2021-11-17 14:30:30 +0530

    Hello World! in Node

---
+++ FirstApp/app.js
@@ -0,0 +1,10 @@
+// to run node app -> node app.js
+/*
+    node is just a runtime environment as like 
+    python provides when use as a scripting lang.
+    Scripting language vs Programming language
+*/
+console.log("Hello World!");
+
+// In nodejs window and document doesn't exist
+console.log(window);
