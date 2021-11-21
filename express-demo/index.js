96144ad580719f34bf12532380fa7062bdf7a665
k-nautresurrect <kushignite@gmail.com>
2021-11-21 22:08:13 +0530

    Environment Variable

--- express-demo/index.js
+++ express-demo/index.js
@@ -12,6 +12,33 @@ app.get('/api/courses',(req,res) => {
 app.get('/api/liveserver',(req,res)=>{
     res.send(['liveserver','new Element'])
 });
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+
+
+// environment variable
+
+/*
+ *
+ * while deploying 3000 may or maynot be available
+ * so it is a good practice to not hard code port number.
+ * 
+*/
+
+// PORT
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 });
+
+/*
+  we can list all the environment variable or paths by
+  export -p
+  here as i have made an env variable port so,
+    declare -x PWD="/home/kaynaut/Desktop/LearnNode/express-demo"
+  this line is there for providing a path to this directory
+  also when i will enter the port number in the console 
+  a new line will be added.
+    declare -x PORT="8080"
+  to reset the port 
+    unset PORT
+*/
